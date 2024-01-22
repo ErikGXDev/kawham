cdd57b30004854f95fb507bbc092be865974662f
ErikoX <54033728+ErikoXDev@users.noreply.github.com>
2024-01-22 18:49:44 +0100

    Refactoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: {
+          root: "#1D2026",
+          default: "#292E37",
+          higher: "#353B47",
+          highest: "#474D5B",
+        },
+        foreground: {
+          muted: "#A9A9A9",
+          default: "#D9D9D9",
+          higher: "#FFFFFF",
+        },
+        primary: {
+          default: "#1578EB",
+        },
+        active: {
+          default: "#F36C33",
+        },
+      },
+    },
   },
   plugins: [],
 };
