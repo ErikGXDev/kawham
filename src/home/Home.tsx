b01de835570d86c9cf8d493748293e32b7a3dc3b
ErikoX <54033728+ErikoXDev@users.noreply.github.com>
2024-01-22 16:11:35 +0100

    Refactor Page Components

---
+++ src/home/Home.tsx
@@ -0,0 +1,16 @@
+import "./Home.scss";
+
+function Home() {
+  return (
+    <main className="container">
+      <div className="sidebar">Sidebar 1</div>
+      <div className="mainwindow">
+        <div>Main Kaboom Game Window</div>
+        <div>File Explorer?</div>
+      </div>
+      <div className="sidebar">Sidebar 2</div>
+    </main>
+  );
+}
+
+export default Home;
