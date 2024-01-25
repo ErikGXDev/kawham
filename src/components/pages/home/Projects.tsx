542ec3cba64a519375ff245653a29727e4a52d74
ErikoX <54033728+ErikoXDev@users.noreply.github.com>
2024-01-25 06:51:24 +0100

    Add "New Project" Button + unfinished modal

--- src/components/pages/home/Projects.tsx
+++ src/components/pages/home/Projects.tsx
@@ -1,11 +1,37 @@
+import Button from "@components/rui/Button";
+import Modal from "@components/rui/Modal";
+import Text from "@components/rui/Text";
+import { useState } from "react";
+
 export default function Projects() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div>
+    <div className="w-full">
       <div className="flex justify-between">
-        <h2 className="text-3xl font-bold">Projects</h2>
+        <Text className="font-bold" variant="largeheader">
+          Projects
+        </Text>
+        <Button
+          variant="filled"
+          className="px-12 h-16"
+          onClick={() => {
+            setShowModal(true);
+          }}
+        >
+          <Text variant="header">New +</Text>
+        </Button>
       </div>
 
       <a href="/editor">temporary link to editor</a>
+
+      <Modal
+        showModal={showModal}
+        setShowModel={setShowModal}
+        title="Create Project"
+      >
+        <Text>Hello!</Text>
+      </Modal>
     </div>
   );
 }
