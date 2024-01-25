41c7814296854278d8ba486ba71b8400ee1101a0
ErikoX <54033728+ErikoXDev@users.noreply.github.com>
2024-01-25 16:00:42 +0100

    Create Project Modal

--- src/components/pages/home/Projects.tsx
+++ src/components/pages/home/Projects.tsx
@@ -1,4 +1,7 @@
+import FolderPicker from "@components/picker/FolderPicker";
 import Button from "@components/rui/Button";
+import Gap from "@components/rui/Gap";
+import Input from "@components/rui/Input";
 import Modal from "@components/rui/Modal";
 import Text from "@components/rui/Text";
 import { useState } from "react";
@@ -8,29 +11,45 @@ export default function Projects() {
 
   return (
     <div className="w-full">
-      <div className="flex justify-between">
+      <div className="flex justify-between items-center">
         <Text className="font-bold" variant="largeheader">
           Projects
         </Text>
-        <Button
-          variant="filled"
-          className="px-12 h-16"
-          onClick={() => {
-            setShowModal(true);
-          }}
-        >
-          <Text variant="header">New +</Text>
-        </Button>
+        <div>
+          <Button
+            variant="filled"
+            onClick={() => {
+              setShowModal(true);
+            }}
+          >
+            <Text variant="text">New +</Text>
+          </Button>
+        </div>
       </div>
 
       <a href="/editor">temporary link to editor</a>
 
       <Modal
         showModal={showModal}
-        setShowModel={setShowModal}
+        setShowModal={setShowModal}
         title="Create Project"
       >
-        <Text>Hello!</Text>
+        <Text variant="subheader">Project Name</Text>
+        <Input placeholder="Bean Platformer" type="text"></Input>
+        <Gap gapsize="small"></Gap>
+        <Text variant="subheader">Folder Path</Text>
+        <FolderPicker></FolderPicker>
+        <Gap gapsize="medium"></Gap>
+        <div className="flex justify-between">
+          <Text>Kawham Version:</Text>
+          <Text>0.1.0</Text>
+        </div>
+        <div className="flex justify-between">
+          <Text>Kaboom Version:</Text>
+          <Text>3000</Text>
+        </div>
+        <Gap gapsize="medium"></Gap>
+        <Button>Create Project</Button>
       </Modal>
     </div>
   );
