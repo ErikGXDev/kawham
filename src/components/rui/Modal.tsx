b0a8366eea9925f5f5fbee5017be12adcb3bbc28
ErikoX <54033728+ErikoXDev@users.noreply.github.com>
2024-01-25 15:30:23 +0100

    Minor spelling mistake also changed ref

--- src/components/rui/Modal.tsx
+++ src/components/rui/Modal.tsx
@@ -4,16 +4,15 @@ import { InputHTMLAttributes, forwardRef } from "react";
 
 export interface ModalProps extends InputHTMLAttributes<HTMLDivElement> {
   showModal: boolean;
-  setShowModel: Function;
+  setShowModal: Function;
   children: React.ReactNode;
   title: string;
 }
 
 const Modal = forwardRef(({ ...props }: ModalProps, ref) => (
-  <div className={cn(props.showModal ? "block" : "hidden")}>
+  <div className={cn(props.showModal ? "block" : "hidden")} ref={ref as any}>
     <div
       {...props}
-      ref={ref as any}
       className="bg-background-default border-4 border-background-highest fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10 rounded-xl overflow-hidden min-w-[400px] min-h-[500px]"
     >
       <div className="border-b-4 border-b-background-highest bg-background-higher flex justify-between py-2 px-4">
@@ -24,7 +23,7 @@ const Modal = forwardRef(({ ...props }: ModalProps, ref) => (
     <div
       className="bg-black/25 w-screen h-screen fixed left-0 top-0"
       onClick={() => {
-        props.setShowModel(false);
+        props.setShowModal(false);
       }}
     ></div>
   </div>
