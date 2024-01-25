581ec7923cd6aeb268392aefbaf05239adcb71c7
ErikoX <54033728+ErikoXDev@users.noreply.github.com>
2024-01-25 11:09:21 +0100

    Make readJSONFile() safer

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -5,9 +5,14 @@ import {
   FsOptions,
 } from "@tauri-apps/api/fs";
 
-export async function readJSONFile<T>(filename: string, options?: FsOptions) {
+export async function readJSONFile<T>(
+  filename: string,
+  fallback: any = {},
+  options?: FsOptions
+) {
   if (!fs.exists(filename, options)) {
-    writeJSONFile(filename, "{}", options);
+    writeJSONFile(filename, fallback, options);
+    return fallback;
   }
 
   let j = await fs.readTextFile(filename, options);
@@ -17,7 +22,7 @@ export async function readJSONFile<T>(filename: string, options?: FsOptions) {
 
 export async function writeJSONFile(
   filename: string,
-  content: string,
+  content: any,
   options?: FsOptions
 ) {
   return fs.writeTextFile(filename, JSON.stringify(content, null, 2), options);
