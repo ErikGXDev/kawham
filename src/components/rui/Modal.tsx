06ad688503c8368e338672308b2075da34dd5be6
ErikoX <54033728+ErikoXDev@users.noreply.github.com>
2024-01-25 06:55:51 +0100

    Make Modal position fixed instead of absolute

--- src/components/rui/Modal.tsx
+++ src/components/rui/Modal.tsx
@@ -14,7 +14,7 @@ const Modal = forwardRef(({ ...props }: ModalProps, ref) => (
     <div
       {...props}
       ref={ref as any}
-      className="bg-background-higher border-4 border-background-highest absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10 rounded-xl min-w-[400px] min-h-[500px]"
+      className="bg-background-higher border-4 border-background-highest fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10 rounded-xl min-w-[400px] min-h-[500px]"
     >
       <div className="border-b-4 border-b-background-highest flex justify-between py-2 px-4">
         <Text variant="text">{props.title}</Text>
@@ -22,7 +22,7 @@ const Modal = forwardRef(({ ...props }: ModalProps, ref) => (
       <div className="h-full p-4">{props.children}</div>
     </div>
     <div
-      className="bg-black/10 w-screen h-screen absolute left-0 top-0"
+      className="bg-black/10 w-screen h-screen fixed left-0 top-0"
       onClick={() => {
         props.setShowModel(false);
       }}
