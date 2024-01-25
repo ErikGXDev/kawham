feb80c3031eced97f4e09b8c2589b5d2bd9acc8e
ErikoX <54033728+ErikoXDev@users.noreply.github.com>
2024-01-25 16:01:35 +0100

    Gap Component

---
+++ src/components/rui/Gap.tsx
@@ -0,0 +1,29 @@
+import { cn } from "@utils/cn";
+import { forwardRef } from "react";
+
+export interface GapProps extends React.HTMLProps<HTMLDivElement> {
+  gapsize: "small" | "medium" | "large";
+  direction?: "horizontal" | "vertical";
+}
+
+const Gap = ({ ...props }: GapProps) => (
+  <span
+    {...props}
+    className={cn(
+      props.className,
+      props.gapsize == "small"
+        ? props.direction == "horizontal"
+          ? "px-2"
+          : "py-2"
+        : props.gapsize == "large"
+        ? props.direction == "horizontal"
+          ? "px-6"
+          : "py-6"
+        : props.direction == "horizontal"
+        ? "px-4"
+        : "py-4",
+    )}
+  ></span>
+);
+
+export default Gap;
