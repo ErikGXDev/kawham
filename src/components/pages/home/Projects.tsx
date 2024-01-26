af34aeedeefb557675b801988baf3f6d48b57959
ErikoX <54033728+ErikoXDev@users.noreply.github.com>
2024-01-26 18:51:33 +0100

    Project Creation and List

--- src/components/pages/home/Projects.tsx
+++ src/components/pages/home/Projects.tsx
@@ -4,41 +4,74 @@ import Gap from "@components/rui/Gap";
 import Input from "@components/rui/Input";
 import Modal from "@components/rui/Modal";
 import Text from "@components/rui/Text";
-import { useState } from "react";
+import { ProjectsFile, createProject, getProjects } from "@utils/project";
+import { useEffect, useRef, useState } from "react";
 
 export default function Projects() {
   const [showModal, setShowModal] = useState(false);
 
+  const projectName = useRef<HTMLInputElement>(null);
+  const projectPath = useRef<HTMLInputElement>(null);
+
+  const [projects, setProjects] = useState<ProjectsFile>([]);
+
+  useEffect(() => {
+    getProjects().then((projects) => {
+      setProjects(projects);
+    });
+  }, []);
+
   return (
     <div className="w-full">
-      <div className="flex justify-between items-center">
-        <Text className="font-bold" variant="largeheader">
-          Projects
-        </Text>
-        <div>
-          <Button
-            variant="filled"
-            onClick={() => {
-              setShowModal(true);
-            }}
-          >
-            <Text variant="text">New +</Text>
-          </Button>
+      <div className="flex flex-col gap-8 h-full">
+        <div className="flex justify-between items-center">
+          <Text className="font-bold" variant="largeheader">
+            Projects
+          </Text>
+          <div>
+            <Button
+              variant="filled"
+              onClick={() => {
+                setShowModal(true);
+              }}
+            >
+              <Text variant="text">New +</Text>
+            </Button>
+          </div>
         </div>
+
+        <ul className="border-2 flex-grow border-background-highest rounded-md overflow-hidden">
+          {projects.length > 0 && (
+            <div className="[&>*:nth-child(odd)]:bg-background-higher rounded-md">
+              {projects.map((project) => (
+                <li className="p-4 px-6 cursor-pointer hover:brightness-105">
+                  <Text variant="subheader">{project.name}</Text>
+                  <Text foreground="dimmer">{project.path}</Text>
+                </li>
+              ))}
+            </div>
+          )}
+          {projects.length == 0 && (
+            <div className="w-full h-full flex items-center justify-center">
+              <Text>Click the "New +" button to create a new project.</Text>
+            </div>
+          )}
+        </ul>
       </div>
-
-      <a href="/editor">temporary link to editor</a>
-
       <Modal
         showModal={showModal}
         setShowModal={setShowModal}
         title="Create Project"
       >
         <Text variant="subheader">Project Name</Text>
-        <Input placeholder="Bean Platformer" type="text"></Input>
+        <Input
+          placeholder="Bean Platformer"
+          type="text"
+          ref={projectName}
+        ></Input>
         <Gap gapsize="small"></Gap>
         <Text variant="subheader">Folder Path</Text>
-        <FolderPicker></FolderPicker>
+        <FolderPicker ref={projectPath}></FolderPicker>
         <Gap gapsize="medium"></Gap>
         <div className="flex justify-between">
           <Text>Kawham Version:</Text>
@@ -49,7 +82,16 @@ export default function Projects() {
           <Text>3000</Text>
         </div>
         <Gap gapsize="medium"></Gap>
-        <Button>Create Project</Button>
+        <Button
+          onClick={() => {
+            let name = projectName.current!.value;
+            let path = projectPath.current!.value;
+            console.log(name, path);
+            createProject(name, path);
+          }}
+        >
+          Create Project
+        </Button>
       </Modal>
     </div>
   );
